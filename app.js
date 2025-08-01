efa6d2008dd64f333db80a07faedbe2a51fca578
dgKim1 <kimdg3556@naver.com>
2025-08-02 00:21:51 +0900

    백엔드 레파지토리 제출

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const app = express();
+
+require("dotenv").config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //req.body가 객체로 인식됨
+
+app.use("/api", indexRouter);
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("mongoose connected"))
+  .catch((err) => console.log("DB connection failed", err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server on");
+});
---
+++ controllers/auth.controller.js
@@ -0,0 +1,26 @@
+const User = require("../Model/User");
+const bcrypt = require("bcryptjs");
+
+const authController = {};
+
+authController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        const token = await user.generateToken();
+        return res.status(200).json({ status: "success", user, token });
+      }
+    }
+    throw new Error("invalid Email or password");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = authController;
