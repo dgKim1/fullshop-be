04c90172ffc0387ff53a427047f0d25ee7aaa320
dgKim1 <kimdg3556@naver.com>
2025-08-05 00:55:47 +0900

    백엔드 토큰 로그인 구현

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const User = require("../Model/User");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const authController = {};
 
 authController.loginWithEmail = async (req, res) => {
@@ -20,6 +20,33 @@ authController.loginWithEmail = async (req, res) => {
       status: "fail",
       error: error.message,
     });
+  }
+};
+
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) throw new Error("Token not found");
+    const token = tokenString.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (error, payload) => {
+      if (error) throw new Error("invalid token");
+      req.userId = payload._id;
+    });
+    next();
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+authController.checkAdminPermission = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+    console.log(user);
+    if (user.level !== "admin") throw new Error("no permission");
+    next();
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
   }
 };
 
